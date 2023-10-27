4933ea487a1241b8dbe56c870030f35d6359534a
FeyzaO-H04 <feyzaoh04.kodehode@gmail.com>
2023-10-27 09:00:08 +0200

    error in Fetch

--- src/assets/Hooks/useFetch.js
+++ src/assets/Hooks/useFetch.js
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
 
+
 export function useFetch(apiUrl) {
 
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-
   const [isLoading, setIsLoading] = useState(true);
-
 
   useEffect(
     () => {
       fetch(apiUrl)
         .then((response) => response.json())
-        .then((fetchedData) => {
+        .then((data) => {
           // Filter and extract the data for books with covers
-          const entries = (fetchedData.entries || []).filter((book) => book.covers && book.covers[0])
+          const entries = (data.entries || []).filter((book) => book.covers && book.covers[0])
           // Start from the 5th book 
           const slicedData = entries.slice(4);
           setData(slicedData);
@@ -26,15 +25,18 @@ export function useFetch(apiUrl) {
         })
     }, 
     []
-  )
+    )
 
-  return (
-    data,
-    error,
-    isLoading
-
-  )
+    return {
+      data,
+      error,
+      isLoading
+    }
   }
  
+  
+
+
+
     
   
--- src/components/Main/bookList.jsx
+++ src/components/Main/bookList.jsx
@@ -1,10 +1,8 @@
+/*
 import { useEffect, useState } from "react";
 import { Loading } from "../Loading/Loading"
 import { ErrorMessage } from "../ErrorMessage/ErrorMessage"
 import { BookCard } from "../BookCard/BookCard"
-
-
-
 import "./bookList.css";
 
 export function BookList() {
@@ -61,7 +59,45 @@ export function BookList() {
   );
 }
 
+*/
+
+import { Loading } from "../Loading/Loading"
+import { ErrorMessage } from "../ErrorMessage/ErrorMessage"
+import { BookCard } from "../BookCard/BookCard"
+import { useFetch } from "../../assets/Hooks/useFetch"
+
+import "./bookList.css";
+
+export function BookList() {
 
 
+  const apiUrl = "https://openlibrary.org/works/OL45804W/editions.json?limit=20";
+
+  const {data, isLoading, error} = useFetch(apiUrl)
+
+  console.log(data)
+
+  return (
+    <div className="mainContent">
+      <div className="bookContainer">
+        {
+          isLoading
+            ? <Loading />
+            : error ? <ErrorMessage />
+              :
+            <ul>
+               {
+                data.map((book, id) => (
+                  <li key={id} className="bookCard">
+                    <BookCard book={book}/>
+                  </li>
+                ))
+               }
+            </ul>
+        }
+      </div>
+    </div>
+  );
+}
 
 
